cb4f3c0ce45cdb0818b595917e309ca388a962e2
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-09-16 04:29:03 +0000

    Add _unsetenv(..) - deletes the variable from the enviroment.

--- env_helper2.c
+++ env_helper2.c
@@ -33,5 +33,50 @@ int _setenv(const char *name, const char *value)
 	return (1);
 }
 
+/**
+ * _unsetenv - deletes the variable name from the environment.
+ *		If name does not exist in the environment,
+ *		then the function succeeds,and the environment is unchanged.
+ * @name: name of the enviroment variable to be deleted.
+ *
+ * Return: (1) on success, (0) otherwise.
+ */
+int _unsetenv(const char *name)
+{
+	char **env = environ, **new_env, *env_record, *key;
+	int i, is_name_in_env = 0;
 
+	for (i = 0; (env[i] != NULL && !is_name_in_env); i++)
+	{
+		env_record = _strdup(env[i]);
+		key = _strtok(env_record, "=");
+		if (_are_strs_eql(key, name))
+			is_name_in_env = 1;
+		free(env_record);
+	}
 
+	if (!is_name_in_env)
+		return (1);
+
+	i = 0;
+	while (env[i])
+		i++;
+	new_env = malloc((sizeof(char *) * (i - 1)) + sizeof(NULL));
+	if (!new_env)
+		return (0);
+	for (i = 0; env[i]; i++)
+	{
+		env_record = _strdup(env[i]);
+		key = _strtok(env_record, "=");
+		if (!(_are_strs_eql(key, name)))
+			new_env[i] = _strdup(env[i]);
+		free(env_record);
+	}
+
+	new_env[i - 1] = NULL;
+
+	free_env(env);
+	environ = new_env;
+
+	return (1);
+}
--- shell.h
+++ shell.h
@@ -45,6 +45,7 @@ void free_env(char **env);
 int modify_record_in_env(const char *name, const char *value, char **env);
 int add_new_record_to_env(const char *name, const char *value, char ***envptr);
 int _setenv(const char *name, const char *value);
+int _unsetenv(const char *name);
 
 int _pow(int, int);
 
