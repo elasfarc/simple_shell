0f735201ed00d1b9f9780e16a5d20cd02289ebc9
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-09-21 06:23:55 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,18 @@
 
 #include <signal.h>
 
-
+/**
+ * sig_handler - Signal Handler Function
+ * @sig: The signal number received
+ *
+ * Description:
+ * This function serves as a signal handler. It is called when a specific
+ * signal, indicated by the `sig` parameter, is received by the program.
+ * The main purpose of this handler is to perform cleanup operations,
+ * releasing allocated memory, and then exit the program with the given signal.
+ *
+ * Return: nothing
+ */
 void sig_handler(int sig)
 {
 	clean_allocated_memory();
